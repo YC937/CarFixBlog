014ed1c132dbf8df4b05faef2d86e8d048676ddd
mserafin20 <mmserafin20@gmail.com>
2023-06-26 21:13:21 -0400

    adjusted syntax in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,7 @@
---DROP DATABASE
+-- Drops database if it exists and creates a new one 
 DROP DATABASE IF EXISTS travelBlog_db;
-
---CREATE DATABASE
 CREATE DATABASE travelBlog_db;
+
+-- Uses the database 
+USE travelBlog_db;
+
