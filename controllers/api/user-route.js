8f56576346dc3ad91761a43c0d84ee2bfd0d026a
unknown <reachout1220@gmail.com>
2023-06-27 22:05:10 -0400

    Modified user-route

--- controllers/api/user-route.js
+++ controllers/api/user-route.js
@@ -1,5 +1,13 @@
-const.router = require('express').Router();
+const router = require('express').Router();
 
+const { User, Post, Comment} = require('../../models');
 
+router.get('/', async (req, res) => {
+    try {
+        const dbUserData = await User.findAll({
+            attribute: {exclude:['password']}, 
+        })
+    }
+})
 
 module.export = router;
