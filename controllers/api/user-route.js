25c009094b7ebbf13bae7d3342530a5803298ce5
ftahira1 <123195285+ftahira1@users.noreply.github.com>
2023-06-30 09:01:50 -0400

    Update connection

--- controllers/api/user-route.js
+++ controllers/api/user-route.js
@@ -11,8 +11,6 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
-<<<<<<< HEAD
-=======
 
 router.get('/:id', async (req, res) => {
     try {
@@ -98,6 +96,5 @@ router.post('/logIn', async (req, res) => {
         res.status(500).json(err);
     }
 });
->>>>>>> b682ce8597963378e7340607695b456c36212f9c
 
 module.export = router;
